cc95f79800c2d06a9b131865b220e2b0cb660686
Igor Shabunin <89274349+shabunin1811@users.noreply.github.com>
2023-04-23 22:44:25 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
+let tg = window.Telegram.WebApp;
 
+tg.expand();
+let data = "";
+
+let btn1 = document.getElementById("item-1");
+
+btn1.addEventListener("click", function(){
+   data = "1";
+   tg.sendData(data);
+});
